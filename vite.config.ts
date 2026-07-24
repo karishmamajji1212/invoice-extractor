8fb16870538f5a7a262189e16e8205b9df0d460f
Ratnakar <karishmamajji12@gmail.com>
2026-07-24 08:16:43 +0530

    update base url

--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,11 @@ import { fileURLToPath, URL } from "node:url";
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
   // Target http://localhost:8000 when local, and production backend URL when in production
+  const env = loadEnv(mode, process.cwd(), "");
+
   const targetUrl =
-    import.meta.env.VITE_API_BASE_URL ||
+    env.VITE_API_BASE_URL ||
+    process.env.VITE_API_BASE_URL ||
     (mode === "production"
       ? "https://invoice-extractor-g0g6.onrender.com"
       : "http://localhost:8000");
